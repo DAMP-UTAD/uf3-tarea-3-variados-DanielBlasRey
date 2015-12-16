b65e1956a31146e9656710728aedd7965eab54f9
Marga <Marga@192.168.1.135>
2015-12-16 18:32:59 +0100

    Sexto commit. Ejercicios del 1 al 13 hechos y además el 26.

---
+++ Uf3-Tarea3/src/Ejercicio26/Ejercicio26.java
@@ -0,0 +1,14 @@
+package Ejercicio26;
+
+public class Ejercicio26 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for (int i = 0; i <= 50; i+=5) {
+			System.out.println("5 x " + num + " = " + i);
+			num++;
+		}
+
+	}
+
+}
