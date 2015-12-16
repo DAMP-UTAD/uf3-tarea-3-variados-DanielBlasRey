61630aef1d57c1b35f6fafbf5b930477ca6abe54
Marga <Marga@192.168.1.135>
2015-12-16 19:26:20 +0100

    Noveno commit. Del ejercicio 1 al 15 y también el 18 y el 26.

---
+++ Uf3-Tarea3/src/Ejercicio18/Ejercicio18.java
@@ -0,0 +1,14 @@
+package Ejercicio18;
+
+public class Ejercicio18 {
+
+	public static void main(String[] args) {
+		int i = 0,num = 0;
+		while (i <= 25) {
+			System.out.println(num += 11);
+			i++;
+		}
+		
+	}
+
+}
